7a35457076297507ba91d6dc0b7c899bb0e2e0da
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-24 17:33:57 +0530

    task5 corrected one

--- src/task5.php
+++ src/task5.php
@@ -1,6 +1,5 @@
 <?php
-$Recorded_temperatures = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
-68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+$Recorded_temperatures = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73";
 $temp_array = explode(',', $Recorded_temperatures);
 $tot_temp = 0;
 $temp_array_length = count($temp_array);
