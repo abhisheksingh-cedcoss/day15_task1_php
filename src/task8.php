6edb78e49d4deaf9058ed7a065713d9cc2647185
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-24 19:05:03 +0530

    task8

--- src/task8.php
+++ src/task8.php
@@ -1,6 +1,12 @@
 <?php
 
-echo "hell";
+$a = array('c1' => 'Red', 'c2' => 'Green', 'c3' => 'White', 'c4' => 'Black');
+$b = array('c2' =>'Green', 'c4'=>'Black');
+
+
+$result = array_diff_key($a,$b);
+
+print_r($result);
 
 
 ?>
