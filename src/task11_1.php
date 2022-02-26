9f6bc869220a1f8a49708f316948196d5192d7a0
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-26 11:54:05 +0530

    cookie reg done

---
+++ src/task11_1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registeration</title>
+</head>
+<body>
+    <form action="task11_2.php" method="post">
+        <label for="name">Name</label>
+        <input type="text" name="name"><br>
+        <label for="email">Email</label>
+        <input type="text" name="email"><br>
+
+        <label for="Phone">Phone</label>
+        <input type="text" name="phone"><br>
+
+        <label for="address">Address</label>
+        <input type="text" name="address"><br>
+
+        <input type="submit" name="submit" value="login">
+
+
+
+
+
+
+
+    </form>
+</body>
+</html>
---
+++ src/task11_2.php
@@ -0,0 +1,18 @@
+<?php
+
+$cookie_name ="user";
+
+$cookie_value =$_POST["name"];
+
+setcookie($cookie_name, $cookie_value, time()+ (86400*30), '/');
+
+?>
+
+<?php
+if(isset($_POST["submit"])){
+    if(isset($_COOKIE[$cookie_name])){
+        echo $_COOKIE[$cookie_name];
+    }
+}
+
+?>
